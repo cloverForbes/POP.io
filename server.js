0460ae5d8f5a27a4fca98503b44ab0038f1d8873
McKinley <solidaritydesignsva@gmail.com>
2017-11-18 19:47:38 -0600

    Allowed for PUT request

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const express  = require('express'),
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Credentials","true");
     next();
 });
 
